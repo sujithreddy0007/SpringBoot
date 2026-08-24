0f59868e7e01afc41440ebd2c94c707433d69de9
Sujith <sujeethreddy0007@gmail.com>
2026-08-24 09:15:58 +0530

    IOC - Removed Commented Lines in IOCExample.java

--- SpringExample/src/main/java/com/ioc/coupling/IOCExample.java
+++ SpringExample/src/main/java/com/ioc/coupling/IOCExample.java
@@ -9,18 +9,15 @@ public class IOCExample {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("applicationIocLooseCouplingExample.xml");
 
-//        UserDataProvider databaseProvider = new UserDatabaseProvider();
-//        UserManager userManagerWithDB = new UserManager(databaseProvider);
+
         UserManager userManagerWithDB = (UserManager) context.getBean("userManagerWithUserDataProvider");
         System.out.println(userManagerWithDB.getUserInfo());
 
-//        UserDataProvider webServiceProvider = new WebServiceDataProvider();
-//        UserManager userManagerWithWS = new UserManager(webServiceProvider);
+
         UserManager userManagerWithWS = (UserManager) context.getBean("userManagerWithWebServiceDataProvider");
         System.out.println(userManagerWithWS.getUserInfo());
 
-//        UserDataProvider newDatabaseProvider = new NewDatabaseProvider();
-//        UserManager userManagerWithNewDB = new UserManager(newDatabaseProvider);
+/
         UserManager newDatabaseProvider = (UserManager) context.getBean("userManagerWithNewDatabaseProvider") ;
         System.out.println(newDatabaseProvider.getUserInfo());
     }
