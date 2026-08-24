5d1e5b4f951b45fbc3598a23f6d6a72151c8d900
Sujith <sujeethreddy0007@gmail.com>
2026-08-24 18:08:23 +0530

    Elimination of XML configurations - moving to Annotations(AppConfig)

---
+++ SpringExample/src/main/java/com.example.componentscan.annotation/Employee.java
@@ -0,0 +1,58 @@
+package com.example.componentscan.annotation;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class Employee {
+    @Value("31")
+    private int employeeId;
+    @Value("Sujith")
+    private String firstName;
+    @Value("Reddy")
+    private String lastName;
+    @Value("1500000")
+    private double salary;
+
+    public int getEmployeeId() {
+        return employeeId;
+    }
+
+    public void setEmployeeId(int employeeId) {
+        this.employeeId = employeeId;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    @Override
+    public String toString() {
+        return "Employee{" +
+                "employeeId=" + employeeId +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", salary=" + salary +
+                '}';
+    }
+}
