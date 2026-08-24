e0e0988fab1caa504447f2715a38272ebb76d069
Sujith <sujeethreddy0007@gmail.com>
2026-08-24 20:12:06 +0530

    REST API - POST with @PostMapping

--- FirstSpring/src/main/java/com/embarkx/FirstSpring/HelloController.java
+++ FirstSpring/src/main/java/com/embarkx/FirstSpring/HelloController.java
@@ -1,6 +1,8 @@
 package com.embarkx.FirstSpring;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -9,4 +11,9 @@ public class HelloController {
     public String hello(){
         return "Hello World!";
     }
+
+    @PostMapping("/hello")
+   public String helloPost(@RequestBody String name){
+        return "Hello " + name +" !";
+   }
 }
