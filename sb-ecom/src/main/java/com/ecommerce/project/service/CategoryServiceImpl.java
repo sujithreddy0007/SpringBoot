eab1ec5beff77f03e264a8359138b93149603814
Sujith <sujeethreddy0007@gmail.com>
2026-08-26 18:47:15 +0530

    Removed logics that resides in CategoryController, just routed and return responses, let bussiness logic will take care by CategoryServiceImpl class

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryServiceImpl.java
@@ -5,24 +5,23 @@ import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.repositories.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.server.ResponseStatusException;
 
-import java.util.ArrayList;
-import java.util.*;
+import java.util.List;
 
 @Service
 public class CategoryServiceImpl implements  CategoryService{
-//    private List<Category> categories = new ArrayList<>();
-//    private Long nextId = 1L;
 
     @Autowired
     private CategoryRepository categoryRepository;
 
     @Override
    public List<Category> getAllCategories(){
-       return categoryRepository.findAll();
+        List<Category> categories = categoryRepository.findAll();
+        if(categories.isEmpty()){
+            throw new APIException("No category created till now.");
+        }
+        return categoryRepository.findAll();
    }
 
     @Override
