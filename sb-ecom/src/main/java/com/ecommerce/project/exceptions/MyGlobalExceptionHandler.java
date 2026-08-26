d38c87b9856811d8cbfae1ede8b21ad5037cbf91
Sujith <sujeethreddy0007@gmail.com>
2026-08-26 18:27:23 +0530

    Implemented APIException with custom bussiness logic and also added findByCategoryName(without writing sql query) in CategoryRepository

--- sb-ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
+++ sb-ecom/src/main/java/com/ecommerce/project/exceptions/MyGlobalExceptionHandler.java
@@ -28,6 +28,13 @@ public class MyGlobalExceptionHandler {
     public ResponseEntity<String> myResourceNotFoundException(ResourceNotFoundException e){
         String message = e.getMessage();
         return new ResponseEntity<>(message,HttpStatus.NOT_FOUND);
-
     }
+
+    @ExceptionHandler(APIException.class)
+    public ResponseEntity<String> myAPIException(APIException e){
+        String message = e.getMessage();
+        return new ResponseEntity<>(message,HttpStatus.BAD_REQUEST);
+    }
+
+
 }
