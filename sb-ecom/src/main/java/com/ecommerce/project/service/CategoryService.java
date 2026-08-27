8f56cfad14a689d8ad238f5e31daa42a996b0fd0
Sujith <sujeethreddy0007@gmail.com>
2026-08-27 20:05:42 +0530

    Implemented sortBy and SortOrder

--- sb-ecom/src/main/java/com/ecommerce/project/service/CategoryService.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -7,7 +7,7 @@ import com.ecommerce.project.payload.CategoryResponse;
 import java.util.List;
 
 public interface CategoryService {
-    CategoryResponse getAllCategories(Integer pageNumber,Integer pageSize);
+    CategoryResponse getAllCategories(Integer pageNumber,Integer pageSize,String sortBy,String sortOrder);
 
     CategoryDTO createCategory(CategoryDTO categoryDTO);
 
