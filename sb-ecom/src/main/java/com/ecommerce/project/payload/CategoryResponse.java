3602480a7af72b760cccf3711add411521dd8f02
Sujith <sujeethreddy0007@gmail.com>
2026-08-27 19:40:53 +0530

    Added responses in getAllCategories - PageNumber,PageSize,TotalElements,TotalPages,isLastPage

--- sb-ecom/src/main/java/com/ecommerce/project/payload/CategoryResponse.java
+++ sb-ecom/src/main/java/com/ecommerce/project/payload/CategoryResponse.java
@@ -11,5 +11,10 @@ import java.util.List;
 @AllArgsConstructor
 public class CategoryResponse {
     private List<CategoryDTO> content;
+    private Integer pageNumber;
+    private Integer pageSize;
+    private Long totalElements;
+    private Integer totalpages;
+    private boolean lastPage;
 
 }
