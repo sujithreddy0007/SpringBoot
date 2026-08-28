9a9c4c86ddc4fe009afb5edbef69c9abc6e9996b
Sujith <sujeethreddy0007@gmail.com>
2026-08-28 09:03:44 +0530

    Implemented Controllers, Repositories, Services and tested with DataInitializer

--- media/src/main/java/com/social/media/models/SocialUser.java
+++ media/src/main/java/com/social/media/models/SocialUser.java
@@ -1,19 +1,24 @@
 package com.social.media.models;
 
 import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.*;
+
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class SocialUser {
     @Id
-    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
     @OneToOne(mappedBy = "user")
+    //@JoinColumn(name = "social_profile_id")
     private SocialProfile socialProfile;
 
     @OneToMany(mappedBy = "socialUser")
@@ -25,6 +30,10 @@ public class SocialUser {
             joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "group_id")
     )
-    private Set<SocialGroup> socialGroups = new HashSet<>();
+    private Set<SocialGroup> groups = new HashSet<>();
 
+    @Override
+    public int hashCode(){
+        return Objects.hash(id);
+    }
 }
