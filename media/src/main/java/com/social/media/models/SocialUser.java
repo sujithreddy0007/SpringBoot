9e4f5875cd723693dc37db81b7748148609120d3
Sujith <sujeethreddy0007@gmail.com>
2026-08-28 07:49:19 +0530

    Implemented One to Many and Many to One Relationships in bidirectional way

--- media/src/main/java/com/social/media/models/SocialUser.java
+++ media/src/main/java/com/social/media/models/SocialUser.java
@@ -2,6 +2,9 @@ package com.social.media.models;
 
 import jakarta.persistence.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Entity
 public class SocialUser {
     @Id
@@ -10,4 +13,7 @@ public class SocialUser {
 
     @OneToOne(mappedBy = "user")
     private SocialProfile socialProfile;
+
+    @OneToMany(mappedBy = "socialUser")
+    private List<Post> posts = new ArrayList<>();
 }
