c9aa7911610559d3fcb4bba9fca5361558e8dbbf
Sujith <sujeethreddy0007@gmail.com>
2026-09-06 03:49:18 +0530

    Added Validations and custom exceptions

--- sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/Product.java
@@ -1,6 +1,8 @@
 package com.ecommerce.project.model;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -13,8 +15,14 @@ public class Product {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private Long productId;
+
+    @NotBlank
+    @Size(min = 3 , message = "Product name must contain atleast 3 characters")
     private String productName;
     private String image;
+
+    @NotBlank
+    @Size(min = 6 , message = "Product description must contain atleast 3 characters")
     private String description;
     private Integer quantity;
     private double price;
--- sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
+++ sb-ecom/src/main/java/com/ecommerce/project/service/ProductServiceImpl.java
@@ -1,6 +1,7 @@
 package com.ecommerce.project.service;
 
 
+import com.ecommerce.project.exceptions.APIException;
 import com.ecommerce.project.exceptions.ResourceNotFoundException;
 import com.ecommerce.project.model.Category;
 import com.ecommerce.project.model.Product;
@@ -45,15 +46,29 @@ public class ProductServiceImpl implements ProductService{
         Category category = categoryRepository.findById(categoryId)
                 .orElseThrow(() ->
                         new ResourceNotFoundException("category","categoryId",categoryId));
-        Product product = modelMapper.map(productDTO,Product.class);
-        product.setImage("Default.png");
-        product.setCategory(category);
-        double specialPrice = product.getPrice() -
-                ((product.getDiscount()*0.01) * product.getPrice());
-        product.setSpecialPrice(specialPrice);
-        Product savedProduct =  productRepository.save(product);
 
-        return modelMapper.map(savedProduct, ProductDTO.class);
+        boolean isProductNotPresent = true;
+        List<Product> products = category.getProducts();
+        for(Product product : products){
+            if(product.getProductName().equals(productDTO.getProductName())){
+                isProductNotPresent = false;
+                break;
+            }
+        }
+
+        if(isProductNotPresent) {
+            Product product = modelMapper.map(productDTO, Product.class);
+            product.setImage("Default.png");
+            product.setCategory(category);
+            double specialPrice = product.getPrice() -
+                    ((product.getDiscount() * 0.01) * product.getPrice());
+            product.setSpecialPrice(specialPrice);
+            Product savedProduct = productRepository.save(product);
+
+            return modelMapper.map(savedProduct, ProductDTO.class);
+        }else{
+            throw new APIException("Product Already exists!!");
+        }
     }
 
     @Override
@@ -63,6 +78,9 @@ public class ProductServiceImpl implements ProductService{
         productDTOS = products.stream()
                 .map(product -> modelMapper.map(product, ProductDTO.class))
                 .toList();
+        if(products.isEmpty()){
+            throw new APIException("No Products Exist!!!");
+        }
         ProductResponse productResponse = new ProductResponse();
         productResponse.setContent(productDTOS);
         return productResponse;
