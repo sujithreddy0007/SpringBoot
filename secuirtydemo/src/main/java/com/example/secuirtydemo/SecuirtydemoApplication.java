c9cc7409a1dd1ef03962a1e06b3c30d3f419d63c
Sujith <sujeethreddy0007@gmail.com>
2026-09-13 14:56:11 +0530

    Created structure for implementing Spring Secuirty

---
+++ secuirtydemo/src/main/java/com/example/secuirtydemo/SecuirtydemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.secuirtydemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecuirtydemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SecuirtydemoApplication.class, args);
+	}
+
+}
---
+++ secuirtydemo/src/test/java/com/example/secuirtydemo/SecuirtydemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.secuirtydemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SecuirtydemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
