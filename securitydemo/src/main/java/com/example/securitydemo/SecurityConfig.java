c78acf7e0d31ce6acb42652a5ba6cb4912be7b3f
Sujith <sujeethreddy0007@gmail.com>
2026-09-14 09:10:59 +0530

    Implemented role based Authorization using PreAuthorize annotation

--- securitydemo/src/main/java/com/example/securitydemo/SecurityConfig.java
+++ securitydemo/src/main/java/com/example/securitydemo/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.example.securitydemo;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
@@ -14,6 +15,7 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
+@EnableMethodSecurity
 public class SecurityConfig {
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception{
@@ -32,7 +34,7 @@ public class SecurityConfig {
                 .build();
         UserDetails admin = User.withUsername("admin")
                 .password("{noop}adminPass")
-                .roles("USER")
+                .roles("ADMIN")
                 .build();
         return new InMemoryUserDetailsManager(user1,admin);
     }
