f220d441606d74e6cce8fadbf79330d3548cc7a7
Sujith <sujeethreddy0007@gmail.com>
2026-09-25 19:37:52 +0530

    Implemented AuthTokenFilter with help of JwtUtils functionalities

---
+++ securitydemo/src/main/java/com/example/securitydemo/jwt/AuthTokenFilter.java
@@ -0,0 +1,62 @@
+package com.example.securitydemo.jwt;
+
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.security.web.authentication.WebAuthenticationDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.web.filter.GenericFilterBean;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+public class AuthTokenFilter extends OncePerRequestFilter {
+
+    @Autowired
+    private JwtUtils jwtUtils;
+
+    @Autowired
+    private UserDetailsService userDetailsService;
+
+    @Autowired
+    protected static final Logger logger = LoggerFactory.getLogger(AuthTokenFilter.class);
+
+
+    protected void doFilterInternal(HttpServletRequest request,
+                                    HttpServletResponse response,
+                                    FilterChain filterChain) throws ServletException, IOException {
+        logger.debug("AuthTokenFilter called for URI: {}",request.getRequestURI());
+        try{
+            String jwt = parseJwt(request);
+            if(jwt != null && jwtUtils.validateJwtToken(jwt)){
+                String userName = jwtUtils.getUserNameFromJWTToken(jwt);
+                UserDetails userDetails =  userDetailsService.loadUserByUsername(userName);
+                UsernamePasswordAuthenticationToken authentication = new UsernamePasswordAuthenticationToken(
+                        userDetails,null,userDetails.getAuthorities()
+                );
+                authentication.setDetails(
+                        new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authentication);
+                logger.debug("Roles from JWT: {}",userDetails.getAuthorities());
+            }
+        } catch (Exception e){
+            logger.error("Cannot set user authentication: {}",e);
+        }
+        filterChain.doFilter(request,response);
+    }
+
+    private String parseJwt(HttpServletRequest request) {
+        String jwt = jwtUtils.getJwtFromHeader(request);
+        logger.debug("AuthTokenFilter.java: {}",jwt);
+        return jwt;
+    }
+}
