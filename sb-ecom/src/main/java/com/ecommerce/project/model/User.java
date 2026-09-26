3a325b6c066d1f592070d0053d16fbfce2a11c48
Sujith <sujeethreddy0007@gmail.com>
2026-09-26 16:05:43 +0530

    used unique constraint for columuns in entities

--- sb-ecom/src/main/java/com/ecommerce/project/model/User.java
+++ sb-ecom/src/main/java/com/ecommerce/project/model/User.java
@@ -16,24 +16,32 @@ import java.util.Set;
 @Entity
 @Data
 @NoArgsConstructor
-@Table(name = "users")
+@Table(name = "users",
+        uniqueConstraints = {
+        @UniqueConstraint(columnNames = "username"),
+        @UniqueConstraint(columnNames = "email")
+        })
 public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "user_id")
     private Long userId;
 
     @NotBlank
     @Size(max = 20)
+    @Column(name = "username")
     private String userName;
 
     @NotBlank
     @Size(max = 50)
     @Email
+    @Column(name = "email")
     private String email;
 
     @NotBlank
     @Size(max = 120)
+    @Column(name = "password")
     private String password;
 
     public User(String userName, String email, String password) {
@@ -44,7 +52,7 @@ public class User {
 
     @Getter
     @Setter
-    @ManyToOne(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
     @JoinTable(name = "user_role", joinColumns = @JoinColumn(name = "user_id"),
             inverseJoinColumns = @JoinColumn(name = "role_id")
     )
